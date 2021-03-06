a7c6cba29ff0b6f4db9779158fb81e1a6f962ae5
najtin <>
2021-03-06 22:19:41 +0100

    fix dockerfile: pull from correct directory

--- bin/docker_entrypoint.sh
+++ bin/docker_entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-chown -R build-user:build-user /home/build-user/ir-anthology
+chown -R build-user:build-user /home/build-user/ir-anthology/data
 exec runuser -u build-user -- make

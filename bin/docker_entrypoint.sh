01852a4ffd948150fb2a341f4d58f799c78fb809
najtin <>
2021-03-06 23:49:45 +0100

    add chown of build to entrypoint

--- bin/docker_entrypoint.sh
+++ bin/docker_entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 chown -R build-user:build-user /home/build-user/ir-anthology/data
+chown -R build-user:build-user /home/build-user/ir-anthology/build
 exec runuser -u build-user -- make

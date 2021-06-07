bbf0390dab589afdcefaacc886bb4fa12e5f8d70
najtin <jan.bittner@student.uni-halle.de>
2021-06-07 12:14:58 +0200

    remove unused scripts

--- bin/docker_entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-chown -R build-user:build-user /home/build-user/ir-anthology/data
-chown -R build-user:build-user /home/build-user/ir-anthology/build
-exec runuser -u build-user -- make
